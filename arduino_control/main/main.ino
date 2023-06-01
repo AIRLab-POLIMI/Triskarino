f83f1e853b614dcccedf01f01646043605d9260a
massimiliano <massimiliano.nigro@mail.polimi.it>
2023-06-01 15:23:43 +0200

    merging with arduino changes

--- arduino_control/main/main.ino
+++ arduino_control/main/main.ino
@@ -67,6 +67,10 @@ NewPing sonar[SONAR_NUM] = {
 #define _MAX_SPEED 80 //cm/s
 #define _MAX_ANGULAR 6.28//rad/s
 
+//Variables needed to read data reliably
+const byte numChars = 32;
+char receivedChars[numChars];
+boolean newData = false;
 
 // Configure the motor driver.
 CytronMD motor1(PWM_PWM, _1_1A, _1_1B); // PWM 2A = Pin 8, PWM 2B = Pin 7. Motor 1 : right robot
@@ -107,25 +111,13 @@ void loop() {
   // I have cm[i] filled with information at this point, let's put it in the Json
   fillSonarMsg();
   
-  // I send data to raspberry according to the speed of his while True
-  if(Serial.available() > 0){
-    fillTwistMsg();
-  }
-
+  recvWithStartEndMarkers();
   
   float speedY = twistData[1];
   float speedX = twistData[0];
   float speedTh = twistData[2];
-  Serial.print("SpeedX");
-  Serial.print(speedX * 0.01);
-  Serial.print(",");
-  Serial.print("SpeedY");
-  Serial.print(speedY * 0.01);
-  Serial.print(",");
-  Serial.print("SpeedTh");
-  Serial.print(speedTh * 0.01);
   //If twist message is equal to the default one, the robot does not move
-  if(speedX * 0.01 == 0.0 & speedY * 0.01 == 0.0 & speedTh * 0.01 == 0.0){
+  if(speedX == 0.0 & speedY == 0.0 & speedTh == 0.0){
     Serial.println("Not trying to move");
     virhas.stop();
   }else{
@@ -137,6 +129,38 @@ void loop() {
   //serializeJson(sensor_msg, Serial);
   //Serial.write("\n");
  
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '{';
+    char endMarker = '}';
+    char rc;
+ 
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
 }
 
 void initializeSensorMsg(){
@@ -177,15 +201,21 @@ void moveRobot(){
 }
 
 
+
 void fillTwistMsg(){
-   const auto deser_err = deserializeJson(twist_msg, Serial);
-  // Test if parsing succeeds.
-  if (deser_err) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(deser_err.f_str());
-    return;
-  }
+   if (newData == true) {
+      Serial.println(receivedChars);
+      const auto deser_err = deserializeJson(twist_msg, receivedChars);
+      // Test if parsing succeeds.
+      if (deser_err) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(deser_err.f_str());
+        return;
+      }
+      newData = false;
+    }
 }
+
 
 void fillOdometryMsg(){
   odometryPos[0] = virhas.getPosX();
--- arduino_control/test.py
+++ arduino_control/test.py
@@ -4,21 +4,25 @@ import json
 
 port = '/dev/ttyACM0'
 baud_rate=115200
-timeout=1
-sleep_time = 1
+timeout=3
+sleep_time = 2
 
 if __name__ == "__main__":
     print("Connection starting...")
     ser = serial.Serial(port, baud_rate, timeout=timeout)
-    ser.flush()
+    ser.flushInput()
+    time.sleep(sleep_time)
     print("Connection is " + str(ser))
     #Read from the serial connection to get the json for odometry and sonar and print it 
     while 1: 
-        time.sleep(sleep_time)
         print("Restarting Loop")
         line = ser.readline()
         line = line.decode("utf-8")
         print("line is..." + str(line))
+        line = ser.readline()
+        line = line.decode("utf-8")
+        print("line is..." + str(line))
+        ser.flushInput()
       #  try:
       #      msg = json.loads(line)
      #       print(msg)
