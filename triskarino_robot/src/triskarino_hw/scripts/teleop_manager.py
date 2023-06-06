eec6e67f6ac7a07c8c2aed9a0be09432c1f0c5df
massimiliano <massimiliano.nigro@mail.polimi.it>
2023-06-06 15:14:58 +0200

    committing in order to merge

--- triskarino_robot/src/triskarino_hw/scripts/teleop_manager.py
+++ triskarino_robot/src/triskarino_hw/scripts/teleop_manager.py
@@ -11,7 +11,7 @@ class TeleopManagerNode():
     NODE_NAME = "teleop_manager"
     def __init__(self):
         rospy.init_node("teleop_manager")
-        self.twist_pub = rospy.Publisher('twist', Twist, queue_size=10)
+        self.twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.gamepad = InputDevice(INPUT_PATH)
         rospy.loginfo(self.gamepad)
 
@@ -31,7 +31,7 @@ class TeleopManagerNode():
                 twist_msg.linear.y = event.value / ANALOG_MAX_VALUE
             elif event.code == 3: 
                 #Get the value and put it into twist linear x
-                twist_msg.linear.y = event.value / ANALOG_MAX_VALUE
+                twist_msg.linear.x = event.value / ANALOG_MAX_VALUE
             elif event.code == 2:
                 #Get the value and put it as negative rotation on z axis
                 twist_msg.angular.z = - event.value / BUTTON_MAX_VALUE
